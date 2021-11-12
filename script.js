79d83bd1d462a83ea53ade7012cb9717366e90f0
Erhan <80072131+erhan-namli@users.noreply.github.com>
2021-11-12 19:12:44 +0300

    Revert "Upadet"
    This reverts commit 9a2c55e06338b37390094b935e9792d01feb7508.

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-
-
-
-
-function myFunction() {
-
-    var x = document.getElementsByClassName("to_do_list")[0].childElementCount;
-    var itm = document.getElementsByClassName("input_area");
-
-    var cln = itm[0].cloneNode(true);
-
-    document.getElementsByClassName("to_do_list")[0].appendChild(cln);
-
-    console.log(x);
-  }
-
-  
+console.log("Selam");

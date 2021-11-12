9a2c55e06338b37390094b935e9792d01feb7508
Erhan <80072131+erhan-namli@users.noreply.github.com>
2021-11-12 19:12:35 +0300

    Upadet

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Selam");
+
+
+
+
+function myFunction() {
+
+    var x = document.getElementsByClassName("to_do_list")[0].childElementCount;
+    var itm = document.getElementsByClassName("input_area");
+
+    var cln = itm[0].cloneNode(true);
+
+    document.getElementsByClassName("to_do_list")[0].appendChild(cln);
+
+    console.log(x);
+  }
+
+  
